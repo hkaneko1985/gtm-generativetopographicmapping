8be0ca1147184e14281a53a6e22efed7ae2954d8
yamasakih <yamasaki.phone@gmail.com>
2018-05-19 08:30:19 +0000

    Add unittest for boston data

--- Python/tests/test_k3nerror.py
+++ Python/tests/test_k3nerror.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import unittest
 
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_boston, load_iris
 
 from gtm import gtm
 from k3nerror import k3nerror
@@ -9,7 +9,83 @@ from k3nerror import k3nerror
 
 class K3NErrorTestCase(unittest.TestCase):
 
-    def setUp(self):
+    def test_k3nerror_with_gtm_to_iris(self):
+        shape_of_map = [10, 10]
+        shape_of_rbf_centers = [5, 5]
+        variance_of_rbfs = 4
+        lambdas = 0.001
+        iterations = 300
+        do_display = 0
+
+        iris = load_iris()
+        X = iris.data
+
+        #autoscaling
+        X = (X-X.mean(axis=0)) / X.std(axis=0,ddof=1)
+
+        model = gtm(shape_of_map, shape_of_rbf_centers, variance_of_rbfs,
+                    lambdas, iterations, do_display)
+        model.fit(X)
+
+        responsibilities = model.responsibility(X)
+        means = responsibilities.dot(model.mapgrids)
+
+        k = 10
+        actual = k3nerror(X, means, k)
+        expected = 0.8332434959
+        self.assertAlmostEqual(expected, actual, places=7)
+
+    def test_k3nerror_with_gtm_to_boston(self):
+        shape_of_map = [10, 10]
+        shape_of_rbf_centers = [5, 5]
+        variance_of_rbfs = 4
+        lambdas = 0.001
+        iterations = 300
+        do_display = 0
+
+        boston = load_boston()
+        X = boston.data
+
+        #autoscaling
+        X = (X-X.mean(axis=0)) / X.std(axis=0,ddof=1)
+
+        model = gtm(shape_of_map, shape_of_rbf_centers, variance_of_rbfs,
+                    lambdas, iterations, do_display)
+        model.fit(X)
+
+        responsibilities = model.responsibility(X)
+        means = responsibilities.dot(model.mapgrids)
+
+        k = 10
+        actual = k3nerror(X, means, k)
+        expected = 13.53674187
+        self.assertAlmostEqual(expected, actual, places=7)
+
+
+class K3NErrorRunSpeedTest:
+
+    def setup_to_boston(self):
+        shape_of_map = [10, 10]
+        shape_of_rbf_centers = [5, 5]
+        variance_of_rbfs = 4
+        lambdas = 0.001
+        iterations = 300
+        do_display = 0
+
+        boston = load_boston()
+        self.X = boston.data
+
+        #autoscaling
+        self.X = (self.X-self.X.mean(axis=0)) / self.X.std(axis=0,ddof=1)
+
+        model = gtm(shape_of_map, shape_of_rbf_centers, variance_of_rbfs,
+                    lambdas, iterations, do_display)
+        model.fit(self.X)
+
+        responsibilities = model.responsibility(self.X)
+        self.means = responsibilities.dot(model.mapgrids)
+
+    def setup_to_iris(self):
         shape_of_map = [10, 10]
         shape_of_rbf_centers = [5, 5]
         variance_of_rbfs = 4
@@ -30,14 +106,7 @@ class K3NErrorTestCase(unittest.TestCase):
         responsibilities = model.responsibility(self.X)
         self.means = responsibilities.dot(model.mapgrids)
 
-    def test_k3nerror_with_gtm(self):
-        k = 10
-        actual = k3nerror(self.X, self.means, k)
-        expected = 0.8332434958
-        self.assertAlmostEqual(expected, actual, places=8)
-
-    def get_k3nerror_calculation_speed(self):
-        k = 10
+    def get_k3nerror_calculation_speed_with_k_equals(self, k):
         k3nerror(self.X, self.means, k)
 
 
