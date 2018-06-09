f683f7f77ba02c92f0892b1c1460ce4f7fa98839
Hiromasa Kaneko <31059982+hkaneko1985@users.noreply.github.com>
2018-06-09 14:28:10 +0900

    fit responsibility (R) calc. when sum of R is 0

--- MATLAB/calc_responsibility.m
+++ MATLAB/calc_responsibility.m
@@ -6,8 +6,9 @@ distancebetweenphiWandinputdataset = pdist2( inputdataset, model.phiofmaprbfgrid
 % responsibilities = ((model.beta/2/pi)^(size(inputdataset,2)/2)) * exp(-model.beta/2*(distancebetweenphiWandinputdataset));
 responsibilities = exp(-model.beta/2*(distancebetweenphiWandinputdataset));
 sumresponsibilities = sum( responsibilities, 2 );
-if any(sumresponsibilities) == 0
-   responsibilities = [];
-else
-   responsibilities = responsibilities ./ repmat(sum( responsibilities, 2 ), 1, size(responsibilities,2) );
+if any(sumresponsibilities)
+	zero_sample = find(sumresponsibilities == 0);
+    sumresponsibilities(zero_sample) = 1;
+    responsibilities(zero_sample,:) = 1/size(responsibilities,2);
 end
+responsibilities = responsibilities ./ repmat(sum( responsibilities, 2 ), 1, size(responsibilities,2) );
